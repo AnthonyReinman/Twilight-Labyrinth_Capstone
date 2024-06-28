5dc35a1451e95cde60b896f70fb35251353acc29
David Stearns <davidstearns561@yahoo.com>
2024-06-27 20:42:52 -0400

    Added rooms controller

--- Assets/Scripts/MazeGenerator/Room.cs
+++ Assets/Scripts/MazeGenerator/Room.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class Room : MonoBehaviour
-{
+public class Room : MonoBehaviour {
+
+    public enum Walls {
+        TOP, RIGHT, BOTTOM, LEFT, NONE,
+    }
+
+    GameObject topWall;
+    GameObject rightWall;
+    GameObject bottomWall;
+    GameObject leftWall;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
