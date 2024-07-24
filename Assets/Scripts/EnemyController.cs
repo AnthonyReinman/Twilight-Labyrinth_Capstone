743f05925d31b36cac16f4232b1d7dc145154c1b
David Stearns <davidstearns561@yahoo.com>
2024-07-23 23:46:14 -0400

    Code cleanup

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,8 +11,6 @@ public class EnemyController : MonoBehaviour {
     public float movementSpeed = 5f; // Speed enemy will move through the world
     public float attackRange = 10f; // Max distance you can be from player to attack
     public float attackStrength = 2f; // Damage that will be done to player upon successful attack
-    // public int wallDetectDist = 2;
-    // public float wallAvoidDist = 5f;
     public Transform playerObject; // Player transform object
 
     // Private Members
@@ -20,25 +18,6 @@ public class EnemyController : MonoBehaviour {
     private Vector2 _playerDirection; // Direction player is relative to player, used for tracking and following player
     private float _playerDistance;
     private Vector2 nextMove;
-    // private bool isStuck = false;
-    // public int stuckCheckInterval = (60 * 30);
-    // private int stuckCheckCounter = (60 * 30) + 1;
-    // private Vector2 lastPos;
-    // private Vector2 avoidanceDirection;
-    // private bool avoidingWall = false;
-    // public float wallAvoidDist = 5f;
-    // private float avoidTime = 100f;
-    // private float avoidTimer = 0f;
-
-    // public float wallAvoidDist = 1f;
-    // public float avoidTime = 5f;
-    // public float avoidTimer = 0f;
-    // public float waitTime = 2f;
-    // private float cooldownTimer = 0f;
-    // private Vector2 avoidanceDirection;
-    // private bool avoidingWall = false;
-
-    // public float wallDetctDist = 1f;
     UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -61,52 +40,6 @@ public class EnemyController : MonoBehaviour {
             tmpDirection.Normalize();
             _playerDirection = tmpDirection;
             agent.SetDestination(playerObject.position);
-
-            // Debug.DrawLine(_body.position, _body.position + _playerDirection * 2, Color.yellow);
-
-            // RaycastHit2D hit = Physics2D.Raycast(_body.position, _playerDirection, 2, LayerMask.GetMask("Wall"));
-            // if (hit.collider != null) {
-            //     Vector2 avoidanceDirection = Vector2.Perpendicular(hit.normal) * 1f;
-            //     nextMove = (_playerDirection + avoidanceDirection).normalized;
-            // } else {
-            //     nextMove = _playerDirection;
-            // }
-
-            // RaycastHit2D hit;
-
-            // if (avoidingWall) {
-            //     hit =  Physics2D.Raycast(_body.position, avoidanceDirection, wallAvoidDist, LayerMask.GetMask("Wall"));
-            //     Debug.DrawLine(_body.position, _body.position + avoidanceDirection * wallAvoidDist, Color.yellow);
-            // } else {
-            //     hit = Physics2D.Raycast(_body.position, _playerDirection, 2, LayerMask.GetMask("Wall"));
-            //     Debug.DrawLine(_body.position, _body.position + _playerDirection * 2, Color.yellow);
-            // }
-
-            // if (hit.collider != null) {
-            //     if (!avoidingWall) {
-            //         avoidanceDirection = (Random.value > 0.5f ? Vector2.Perpendicular(_playerDirection) : -Vector2.Perpendicular(_playerDirection)).normalized;
-            //         avoidingWall = true;
-            //         avoidTimer = avoidTime;
-            //     }
-            //     nextMove = (_playerDirection + avoidanceDirection * wallAvoidDist).normalized;
-            //     avoidTimer -= Time.deltaTime;
-            //     if (avoidTimer <= 0) {
-            //         avoidingWall = false;
-            //     }
-            // } else {
-            //     nextMove = _playerDirection;
-            //     avoidingWall = false;
-            // }
-
-            // nextMove = _playerDirection;
-            // RaycastHit2D rcHit = Physics2D.Raycast(transform.position, nextMove, wallDetctDist, wallLayer);
-            // Debug.DrawLine(_body.position, _body.position + nextMove * wallDetctDist, Color.yellow);
-            // if (rcHit.collider != null) {
-            //     Vector2 wallNml = rcHit.normal;
-            //     Vector2 avoidDirc = Vector2.Perpendicular(wallNml).normalized;
-            //     nextMove = avoidDirc;
-            // }
-
 
             // Get distance to player
             _playerDistance = Vector2.Distance(playerObject.position, _body.position);
@@ -136,10 +69,6 @@ public class EnemyController : MonoBehaviour {
         if (_body != null && _playerDirection != null) {
             // _body.MovePosition((Vector2)_body.position + (nextMove * movementSpeed * Time.fixedDeltaTime));
         }
-
-        // if (agent != null && playerObject != null) {
-        //     agent.SetDestination(playerObject.position);
-        // }
     }
 
     private void AttackPlayer() {
