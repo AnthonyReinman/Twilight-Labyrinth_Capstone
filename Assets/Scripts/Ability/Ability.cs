aa3f27efcc786c3e2368e536f88d0c34e64c556d
Anthony Judd Reinman <ajreinman@student.fullsail.edu>
2024-08-11 10:20:48 -0700

    Camera Zoom Ability

--- Assets/Scripts/Ability/Ability.cs
+++ Assets/Scripts/Ability/Ability.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+public abstract class Ability : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string abilityName;
+    public int level = 1;
+    public int maxLevel = 5;
+    public bool isActive = false; // Tracks if the ability is active
 
-    // Update is called once per frame
-    void Update()
+    public abstract void Activate();
+    public abstract void Upgrade();
+
+    public void SetActive(bool active)
     {
-        
+        isActive = active;
+        gameObject.SetActive(active);
     }
 }
